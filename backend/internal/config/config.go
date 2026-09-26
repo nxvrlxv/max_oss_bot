1784bff2ed67790a7a86a99eb54b911055ebf8da
nxvrlxv <eldar.tishchenko@yandex.ru>
2026-09-26 12:54:31 -0700

    новая ссылка api max

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -31,7 +31,7 @@ const (
 
 	// Адрес по умолчанию: сертификат platform-api2 выпущен УЦ Минцифры,
 	// без его установки в систему соединение обрывается.
-	defaultAPIBase = "https://platform-api.max.ru/"
+	defaultAPIBase = "https://platform2-api.max.ru/"
 )
 
 func Load() (Config, error) {
